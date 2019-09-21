24c668c4c0866e37b19b1f373a40ab148a2a6579
Hugo de Melo <hsemog@gmail.com>
2019-09-21 18:59:17 +0200

    app to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { FunctionComponent, useEffect } from "react";
 import { Provider } from "react-redux";
 import { Switch, Route, Redirect, BrowserRouter } from "react-router-dom";
 import { dataStore } from "./data/dataStore";
@@ -6,31 +6,27 @@ import { addProduct } from "./data/actionCreators";
 import { ConnectedProductList } from "./components/ProductList";
 import "./App.css";
 
-interface Props {}
-
-class App extends Component<Props> {
-    componentDidMount() {
+const App: FunctionComponent = () => {
+    useEffect(() => {
         (async () => {
             const result = await fetch("http://localhost:4600/products/");
             const products = await result.json();
             dataStore.dispatch(addProduct(...products));
         })();
-    }
+    }, []);
 
-    render() {
-        return (
-            <div className="App">
-                <Provider store={dataStore}>
-                    <BrowserRouter>
-                        <Switch>
-                            <Route path="/products" component={ConnectedProductList} />
-                            <Redirect to="/products" />
-                        </Switch>
-                    </BrowserRouter>
-                </Provider>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="App">
+            <Provider store={dataStore}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/products" component={ConnectedProductList} />
+                        <Redirect to="/products" />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
+        </div>
+    );
+};
 
 export default App;
