97636fc3e8acf88839a36ac3e960a10828094704
Hugo de Melo <hsemog@gmail.com>
2019-10-26 08:27:13 +0200

    use ActionCreator from redux instead of explicit return type

--- src/data/actionCreators.ts
+++ src/data/actionCreators.ts
@@ -1,14 +1,15 @@
 import { ACTIONS, AddProductsAction, ModifyOrderAction, ResetOrderAction } from "./types";
 import { Product } from "./entities";
+import { ActionCreator } from "redux";
 
-export const addProduct = (...products: Product[]): AddProductsAction => {
+export const addProduct: ActionCreator<AddProductsAction> = (...products: Product[]) => {
     return {
         type: ACTIONS.ADD_PRODUCTS,
         payload: products
     };
 };
 
-export const modifyOrder = (product: Product, quantity: number): ModifyOrderAction => {
+export const modifyOrder: ActionCreator<ModifyOrderAction> = (product: Product, quantity: number) => {
     return {
         type: ACTIONS.MODIFY_ORDER,
         payload: {
@@ -18,4 +19,4 @@ export const modifyOrder = (product: Product, quantity: number): ModifyOrderActi
     };
 };
 
-export const resetOrder = (): ResetOrderAction => ({ type: ACTIONS.RESET_ORDER });
+export const resetOrder: ActionCreator<ResetOrderAction> = () => ({ type: ACTIONS.RESET_ORDER });
